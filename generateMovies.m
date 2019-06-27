5c7db45678e18ea2852d4db6793fa81c3b9b1546
foragingBRAIN <33703375+foragingBRAIN@users.noreply.github.com>
2019-06-27 13:13:46 -0700

    .

--- generateMovies.m
+++ generateMovies.m
@@ -1,8 +1,14 @@
 
 
+clear all
+close all
+
+g = gpuDevice(1);
+reset(g);
+
 nFrames = 100;
 stimsize = [10000,10000];
-stimFolder = '~/Documents/stimuli/';
+stimFolder = 'C:\Users\angelakiVR\Documents\stimuli\';
 
 
 %% pink noise texture params
@@ -16,35 +22,35 @@ v = repmat(v,stimsize(1),1);
 
 S_f = (u.^2 + v.^2).^(beta/2); 
 S_f(S_f==Inf) = 0;
-
+S_f = gpuArray(S_f.^0.5);
 % temporal weighting
 speedchange = 20;
 k = sqrt(u.^2 + v.^2);
-invk = exp(-k.*speedchange);
+invk = gpuArray(exp(-k.*speedchange));
 invk(isinf(invk)) = 0;
 
-phi = 2*pi*rand(stimsize);
-
-compSpectrum = (S_f.^0.5) .* (cos(phi) + 1i.*sin(phi));
+oldSpectrum = gpuArray(2*pi*rand(stimsize));
+oldSpectrum = S_f .* (cos(oldSpectrum) + 1i.*sin(oldSpectrum));
 
 for ff=1:nFrames
     fprintf('\nGenerating frames...%i%%',ceil(round(100*ff/nFrames)));
     
-    phi = 2*pi*rand(stimsize);
-    tempSpectrum = (S_f.^0.5) .* (cos(phi) + 1i.*sin(phi));
-    compSpectrum = invk.*compSpectrum + sqrt(1-invk.^2).*tempSpectrum;
+    newSpectrum = gpuArray(2*pi*rand(stimsize));
+    newSpectrum = S_f .* (cos(newSpectrum) + 1i.*sin(newSpectrum));
+    newSpectrum = sqrt(1-invk.^2).*newSpectrum;
+    
+    oldSpectrum = invk.*oldSpectrum;
+    oldSpectrum = oldSpectrum + newSpectrum;
 
-    Xmat = ifft2(compSpectrum);
-%     Xmat = angle(Xmat + colvar*exp(1i*colmean));
-%     Xgray = angle(Xgray + E.stimConcentration.*exp(1i.*(-0.5*pi+0.5*lambdaVec(frameNum)*pi)));
-    Xabs = real(Xmat);
-    Xgray = (Xabs/std(Xabs(:)))*128/3 + 128;
-    Xgray = min(max(round(Xgray),0),255);
-
+    Xmat = ifft2(oldSpectrum);
+    Xmat = real(Xmat);
+    Xmat = Xmat./std(Xmat(:))*128/3+128;
+    Xmat = round(Xmat);
+    Xmat = max(Xmat,0);
+    Xmat = min(Xmat,255);
+    
     fileName = sprintf('%sstimulus%s%i.png',stimFolder,repmat('0',1,5-fix(log10(ff))),ff);
-    
-    imwrite(uint8(Xgray),fileName);
-    
+    imwrite(uint8(gather(Xmat)),fileName);
 end
 
 
